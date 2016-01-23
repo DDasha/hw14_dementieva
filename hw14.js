de877450367936579b7f31ff5526171378df72ff
Daria Dementieva <Daria Dementieva>
2016-01-23 21:46:41 +0200

    third level

--- hw14.js
+++ hw14.js
@@ -15,6 +15,21 @@ isFree('east');
 while (east() ==true) { east(); }
 
 //3level
+map();
+hint();
+isFree('south');
+//command test! Never use do-while loop!!!!!!!!!!!!!! Never!!!!!!!
+
+//command test2
+if (isFree('south')==true) {
+	south();
+} 
+else {
+	east();
+}
+
+map();
+
 
 //4level
 
